457a790cc78f6122c96acfad4ef93f27476b31fe
somanna <pdsomanna@gmail.com>
2019-09-07 10:39:22 -0400

    added

--- info1.py
+++ info1.py
@@ -1,8 +1,8 @@
-import xml.etree.ElementTree as ElementTree
+import xml.etree.ElementTree as ET
 import csv
 tree = ET.parse("info.xml")
 root = tree.getroot()
-resident_data = open("info.csv",'w')
+resident_data = open('info.csv','w') 
 csvwriter = csv.writer(resident_data)
 resident_head = []
 
@@ -11,16 +11,16 @@ for member in root.findall('resident'):
 	resident = []
 	address_list=[]
 	if count == 0:
-		name = member.find('Name').tag
+		name = member.find('name').tag
 		resident_head.append(name)
-		number=member.find('Number').tag
+		number=member.find('number').tag
 		resident_head.append(number)
 		Address=member[2].tag
 		resident_head.append(Address)
 		csvwriter.writerow(resident_head)
 		count=count+1
 
-	name=member.find('Name').text
+	name=member.find('name').text
 	resident.append(name)
 	number=member.find('number').text
 	resident.append(number)
@@ -28,8 +28,7 @@ for member in root.findall('resident'):
 	address_list.append(Address)
 	city = member[2][1].text
 	address_list.append(city)
-	postcode = member[2][2].text
-	address_list.append(postcode)
+	
 	resident.append(address_list)
 	csvwriter.writerow(resident)
 resident_data.close()
